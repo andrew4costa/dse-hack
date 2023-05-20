ae7b2fcbb4b3f736676bd78e1010a6e6c0a0dc6b
Andrew Costa <67336273+andrew4costa@users.noreply.github.com>
2023-05-20 20:44:18 +0200

    Add files via upload

--- gcp/api/train.py
+++ gcp/api/train.py
@@ -1,108 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Local training
-
-# In[1]:
-
-
-import pickle
+########### IMPORT DEPENDENCIES ###########
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.metrics import f1_score
+import pickle
+import sklearn
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.pipeline import Pipeline, make_pipeline
+#from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import VotingClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
 
 
-# ### Load the data
 
-# In[2]:
+########### GET DATA #######################
+df = pd.read_csv('/Users/andrewcosta/Desktop/dse-hackathon/titanic/train.csv')
 
-
-zoo = pd.read_csv("../data/zoo.csv")
-zoo
-
-
-# ### Train/test split
-
-# In[3]:
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_df[features])
+y = train_df["Survived"]
 
 
-X = zoo.iloc[:,1:-1]
-y = zoo.iloc[:,-1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+########### SPLIT DATA ######################
+X_train, X_val, y_train, y_val = train_test_split(X,y, train_size=0.7, random_state=0)
 
 
-# ### Create an ensamble classifier
+########### MODEL SET-UP ####################
+clf1 = RandomForestClassifier()
+clf2 = ExtraTreesClassifier()
 
-# In[4]:
+voting_clf = VotingClassifier(estimators=[('rfc', clf1), ('etc', clf2)])
 
+pipeline = Pipeline([('voting_clf', voting_clf)])
 
-ensamble = VotingClassifier(estimators=[
-    ('mnb', MultinomialNB()),
-    ('svc', SVC()),
-    ('rf', RandomForestClassifier())
-])
-
-
-# ### Create a pipeline
-
-# In[5]:
+# Fit the pipeline to the training data
+pipeline.fit(X_train, y_train)
 
 
-pipe = Pipeline([
-    ('encoder', None), 
-    ('classifier', ensamble),
-])
+########### PREDICTIONS ######################
+predictions = pipeline.predict(X_val)
+accuracy = accuracy_score(y_val, predictions)
 
 
-# ### Optimize parameters
+########### SAVE MODEL #######################
 
-# In[6]:
-
-
-cls = GridSearchCV(
-    pipe, 
-    {
-        'encoder': [
-            None,
-            OneHotEncoder(handle_unknown='ignore')
-        ],
-        'classifier__mnb__alpha': [0.1, 1, 2],
-        'classifier__svc__C': [0.1, 1, 10],
-        'classifier__svc__class_weight': ['balanced'],
-        'classifier__rf__n_estimators': [10, 100],
-        'classifier__rf__criterion': ['gini', 'entropy'],
-    }, 
-    cv=5, 
-    scoring='f1_macro'
-)
-
-
-# In[7]:
-
-
-cls.fit(X_train, y_train)
-cls.best_params_
-
-
-# ### Print evaluation metrics
-
-# In[8]:
-
-
-print('Validation score', cls.best_score_)
-print('Test score', cls.score(X_test, y_test))
-
-
-# ### Save the model
-
-# In[9]:
-
-
-pickle.dump(cls, open('model.pkl', 'wb'))
-
+filename = '/content/drive/MyDrive/titanic/model.pkl'
+pickle.dump(pipeline, open(filename, 'wb'))
